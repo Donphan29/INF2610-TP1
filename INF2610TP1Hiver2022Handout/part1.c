4fbc1be03bb8b471323d9332c177c1df37c79260
DonPhan29 <huy-don.phan@polymtl.ca>
2022-02-07 15:15:49 -0500

    TP1 - calculate nChilds with while loop and start of clean up

--- INF2610TP1Hiver2022Handout/part1.c
+++ INF2610TP1Hiver2022Handout/part1.c
@@ -1,25 +1,24 @@
 /*
- *  part1.c
+ * processlab - part1.c
  * 
- * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
+ * École polytechnique de Montréal, Hiver 2022
+ * Maxence Lefebvre, XXXX 
+ * Huy-Don Phan, 2074318
+ * Section de laboratoire : 02 (B2)
+ * 
  */
 
-// TODO
-// Si besoin, ajouter ici les directives d'inclusion
-// -------------------------------------------------
-
-// -------------------------------------------------
 
 #include "./libprocesslab/libprocesslab.h"
 
 void question1(int choix) {
-    // TODO
     if (choix == 1) {
         printf("75dbcb01f571f1c32e196c3a7d27f62e (printed using printf)");
         write(STDOUT_FILENO, "75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n", strlen("75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n"));
         printf("\n");
     }
-    if (choix == 2) {
+
+    else if (choix == 2) {
         setvbuf(stdout, NULL, _IONBF, 1024);
         printf("75dbcb01f571f1c32e196c3a7d27f62e (printed using printf)");
         write(STDOUT_FILENO, "75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n", strlen("75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n"));
