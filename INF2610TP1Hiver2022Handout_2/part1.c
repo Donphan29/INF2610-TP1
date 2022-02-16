35eeaa3ff6f863168c4b84bc8110a76f20f9165d
Don <huy-don.phan@polymtl.ca>
2022-02-16 14:57:55 -0500

    Add TP2

---
+++ INF2610TP1Hiver2022Handout_2/part1.c
@@ -0,0 +1,19 @@
+/*
+ *  part1.c
+ * 
+ * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
+ */
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+#include "./libprocesslab/libprocesslab.h"
+
+void question1(int choix) {
+    // TODO
+ 
+}
+    
---
+++ INF2610TP1Hiver2022Handout_2/part2.c
@@ -0,0 +1,22 @@
+/*
+ * processlab - part2.c
+ * 
+ * Ecole polytechnique de Montreal,Hiver  2022
+ * vos noms, prénoms et matricules
+*/
+
+#include "libprocesslab/libprocesslab.h"
+
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+void question2( )
+{
+    // TODO
+    
+}
+
