e7d428797580d82316c82ff66f7b6e6ac65b4a97
maxence lefebvre <maxence.lefebvre@polymtl.ca>
2022-02-16 16:44:29 -0500

    TP2 complété

--- TP2_Gr2/TubesAnonymes.c
+++ TP2_Gr2/TubesAnonymes.c
@@ -1,4 +1,65 @@
-// ajouter les directives d'inclusion nécessaires
-int main() {
-    
-}
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/wait.h> 
+#include <fcntl.h>
+
+int main( ) {
+    int pid;
+    // création de deux pipe
+    int fd[2]; pipe(fd);
+    int fd2[2]; pipe(fd2);
+
+    if (fork()==0) {
+        // premier fils
+        int file = open("./In.txt", O_RDONLY);
+
+        dup2(file,0);         
+        dup2(fd[1],1);  
+
+        close(fd[0]); 
+        close(fd[1]); 
+        close(fd2[0]); 
+        close(fd2[1]);  
+
+        execl("/bin/rev","rev",NULL); 
+
+        _exit(1);     
+    }     
+
+    if (fork()==0) {
+        // second  fils            
+        dup2(fd[0],0);
+        dup2(fd2[1],1);   
+
+        close(fd[0]); 
+        close(fd[1]);
+        close(fd2[0]); 
+        close(fd2[1]); 
+
+        execl("/bin/rev","rev", NULL);
+
+        _exit(1);     
+    }
+
+    if (fork()==0) {
+        // troisième  fils            
+        dup2(fd2[0],0);  
+
+        close(fd[0]); 
+        close(fd[1]);
+        close(fd2[0]);
+        close(fd2[1]); 
+
+        execl("/bin/diff","diff", "-", "./In.txt", "-s", NULL);
+
+        _exit(1);     
+    } 
+
+    close(fd[0]); 
+    close(fd[1]);
+    close(fd2[0]); 
+    close(fd2[1]); 
+     
+    while ((pid=wait(NULL)) > 0);    
+    return 0;
+} 
--- TP2_Gr2/TubesNommes.c
+++ TP2_Gr2/TubesNommes.c
@@ -1,4 +1,56 @@
-// ajouter les directives d'inclusion nécessaires
-int main() {
-    
-}
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/wait.h> 
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main( ) {
+    int pid;
+    // création de deux pipe
+    mkfifo("fd", 0660);
+    mkfifo("fd2", 0660);
+
+    if (fork()==0) {
+        // premier fils
+        int file = open("./In.txt", O_RDONLY);
+        int fd = open("fd", O_WRONLY);
+
+        dup2(file,0);         
+        dup2(fd,1);  
+        close(fd);    
+
+        execl("/bin/rev","rev",NULL);       
+
+        _exit(1);     
+    }   
+
+    if (fork()==0) {
+        // second  fils   
+        int fd = open("fd", O_RDONLY); 
+        int fd2 = open("fd2", O_WRONLY);    
+
+        dup2(fd,0);
+        dup2(fd2,1);        
+        close(fd);
+        close(fd2);
+
+        execl("/bin/rev","rev", NULL);
+
+        _exit(1);     
+    }
+
+    if (fork()==0) {
+        // troisième  fils    
+        int fd2 = open("fd2", O_RDONLY);  
+
+        dup2(fd2,0);        
+        close(fd2);
+        
+        execl("/bin/diff","diff", "-", "./In.txt", "-s", NULL);
+
+        _exit(1);     
+    }
+
+    while ((pid=wait(NULL)) > 0);    
+    return 0;
+} 
