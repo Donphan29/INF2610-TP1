97b75b738b75f79152005157e667ae45d32bb04d
DonPhan29 <huy-don.phan@polymtl.ca>
2022-04-06 20:39:28 -0400

    tp4 - clean code and documentation

--- TP4/GestionMemoire.c
+++ TP4/GestionMemoire.c
@@ -9,20 +9,49 @@
  */
 
 #include "./libs/lib.h"
+
+/*
+ * Calcul le numéro de page
+ *
+ * @param adresse virtuelle
+ * @return numéro de page
+ */
 unsigned int calculerNumeroDePage(unsigned long adresse) {
     return adresse / TAILLE_PAGE_CADRE;
 }
 
+/*
+ * Calcul le déplacement dans la page
+ *
+ * @param adresse virtuelle
+ * @return déplacement dans la page
+ */
 unsigned long calculerDeplacementDansLaPage(unsigned long adresse) {
     return adresse % TAILLE_PAGE_CADRE;
 }
 
+/*
+ * Calcul l'adresse complète
+ *
+ * @param numéro de page et déplacement dans la page
+ * @return adresse complète
+ */
 unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage) {
     return numeroDePage * TAILLE_PAGE_CADRE + deplacementDansLaPage;
 }
 
+/*
+ * Recherche une page dans le TLB
+ * 
+ * Si la page se trouve dans le TLB, la requête est assignée une adresse physique
+ * correspondante avec la fonction calculerAdresseComplete. Sinon, celle-ci prend
+ * la valeur 0. Les params sont mis à jour selon les cas.
+ *
+ * @param requête et mémoire
+ */
 void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	int numeroDePageReq = calculerNumeroDePage(req->adresseVirtuelle);
+
 	for (int i = 0; i < TAILLE_TLB; i++) {
 		if (mem->tlb->numeroPage[i] == numeroDePageReq && mem->tlb->entreeValide[i]) {
 			req->adressePhysique = calculerAdresseComplete(mem->tlb->numeroCadre[i], calculerDeplacementDansLaPage(req->adresseVirtuelle));
@@ -35,8 +64,18 @@ void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	req->estDansTLB = 0;
 }
 
+/*
+ * Recherche une page dans la table des pages
+ * 
+ * Si la page se trouve dans la table des pages, la requête est assignée une 
+ * adresse physique correspondante avec la fonction calculerAdresseComplete. 
+ * Sinon, celle-ci prend la valeur 0. La requête est mis à jour selon le cas.
+ *
+ * @param requête et mémoire
+ */
 void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	int numeroDePage = calculerNumeroDePage(req->adresseVirtuelle);
+
 	if (mem->tp->entreeValide[numeroDePage]) {
 		req->adressePhysique = calculerAdresseComplete(mem->tp->numeroCadre[numeroDePage], calculerDeplacementDansLaPage(req->adresseVirtuelle));
 		req->estDansTablePages = 1;
@@ -46,12 +85,23 @@ void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* 
 	}
 }
 
+/*
+ * Ajoute une page dans la mémoire principale
+ * 
+ * Dans le premier cadre disponible, la page précisé par la requête est ajoutée
+ * à la mémoire principale en supposant qu'elle n'est jamais pleine. L'adresse
+ * physique correspondante est calculée et stockée dans la requête et les paramètres
+ * de la mémoire sont mis à jour.
+ * 
+ * @param requête et mémoire
+ */
 void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	int numeroDePage = calculerNumeroDePage(req->adresseVirtuelle);
+
 	for (int i = 0; i < TAILLE_MEMOIRE; i++) {
 		if (mem->memoire->utilisee[i] == 0) {
-			mem->memoire->numeroPage[i] = numeroDePage;
 			req->adressePhysique = calculerAdresseComplete(i, calculerDeplacementDansLaPage(req->adresseVirtuelle));
+			mem->memoire->numeroPage[i] = numeroDePage;
 			mem->memoire->dateCreation[i] = req->date;
 			mem->memoire->dernierAcces[i] = req->date;
 			mem->memoire->utilisee[i] = 1;
@@ -60,6 +110,15 @@ void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) 
 	}
 }
 
+/*
+ * Met à jour une page dans le TLB
+ * 
+ * Si le TLB est plein, une page est remplacée par celle précisée dans la requête
+ * selon la politique FIFO. Sinon, celle-ci sera ajoutée dans le premier cadre
+ * libre. Les autres paramres de la mémoire sont mis à jour.
+ * 
+ * @param requête et mémoire
+ */
 void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	int index = 0;
 	int plein = 0;
@@ -71,14 +130,15 @@ void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 			break;
 		}
 
-		if(i == TAILLE_TLB - 1 & !plein){
-			plein = 1; 
-			i = 0;
-		}
+		if(i == TAILLE_TLB - 1 & !plein) plein = 1; 
+	}
 
-		if (mem->tlb->dateCreation[i] < premierElement & plein) {
-			index = i;
-			premierElement = mem->tlb->dateCreation[i];
+	if (plein) {
+		for (int i = 0; i < TAILLE_TLB; i++) {
+			if (mem->tlb->dateCreation[i] < premierElement & plein) {
+				index = i;
+				premierElement = mem->tlb->dateCreation[i];
+			}
 		}
 	}
 
@@ -89,7 +149,7 @@ void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
 	mem->tlb->entreeValide[index] = 1;
 }
 
-// NE PAS MODIFIER
+/* Fonction main */
 int main() {
     evaluate(
 		&calculerNumeroDePage, 
